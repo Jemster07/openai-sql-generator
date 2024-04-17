5de03952db38b9da642dddcc18695a673ccd849c
Jemster07 <emelie.kerek@gmail.com>
2024-04-17 14:17:02 -0400

    discovered free models of OpenAI have been discontinued

--- server/generate.js
+++ server/generate.js
@@ -1,9 +1,9 @@
 import openaiClient from "./api.js";
 
-const generate = async (queryDescription) => {
-  const response = await openaiClient.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `Convert the following natural language description into a SQL query:\n\n${queryDescription}.`,
+const generate = async (userPrompt) => {
+  const response = await openaiClient.chat.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    prompt: `Convert the following natural language description into a SQL query:\n\n${userPrompt}.`,
     max_tokens: 100,
     temperature: 0,
   });
--- server/index.js
+++ server/index.js
@@ -1,27 +1,29 @@
 import express from "express";
 import cors from "cors";
+import generate from "./generate.js";
 
 const app = express();
+
+app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 3002;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+  res.send("Hello World!");
 });
 
 app.post("/generate", async (req, res) => {
-    const { prompt } = req.query;
-    try {
-      const sqlQuery = await generate(prompt);
-      res.json({ sqlQuery });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
-  
+  const userPrompt = req.body.userPrompt;
+  try {
+    const sqlQuery = await generate(userPrompt);
+    res.json({ response: sqlQuery });
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
